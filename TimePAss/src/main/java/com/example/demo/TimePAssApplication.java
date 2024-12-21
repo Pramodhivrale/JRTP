1401a8c3d41db481fd8ea941c599553c98ea6e3c
Admin <hivralepramod4@gmail.com>
2024-12-21 09:54:37 +0530

    A

---
+++ TimePAss/src/main/java/com/example/demo/TimePAssApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimePAssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimePAssApplication.class, args);
+	}
+
+}
